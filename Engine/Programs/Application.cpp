98ec2caf403fbe43d6171c1853522b8ab60502ef
w6rst <1486008923@qq.com>
2025-07-19 00:56:37 +0800

    Add Application class

---
+++ Engine/Programs/Application.cpp
@@ -0,0 +1,59 @@
+#include "Profiling/Stopwatch.hpp"
+
+#include "Engine.hpp"
+#include "Renderer.hpp"
+
+#include "World.hpp"
+#include "Application.hpp"
+
+namespace worse::pc
+{
+    Application::Application(int argc, char** argv)
+    {
+        (void)argc;
+        (void)argv;
+
+        // clang-format off
+        m_schedule.addSystem<ecs::CoreStage::StartUp, Engine::initialize>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, Renderer::initialize>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, ImGuiRenderer::initialize>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, World::initialize>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, World::setupScene>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, buildMeshes>();
+        m_schedule.addSystem<ecs::CoreStage::StartUp, buildMaterials>();
+
+        m_schedule.addSystem<ecs::CoreStage::Update, Engine::tick>();
+        m_schedule.addSystem<ecs::CoreStage::Update, World::updateInput>();
+        m_schedule.addSystem<ecs::CoreStage::Update, World::update>();
+        m_schedule.addSystem<ecs::CoreStage::Update, buildDrawcalls>();
+        m_schedule.addSystem<ecs::CoreStage::Update, ImGuiRenderer::tick>();
+        m_schedule.addSystem<ecs::CoreStage::Update, Renderer::tick>();
+
+        m_schedule.addSystem<ecs::CoreStage::CleanUp, ImGuiRenderer::shutdown>();
+        m_schedule.addSystem<ecs::CoreStage::CleanUp, Renderer::shutdown>();
+        m_schedule.addSystem<ecs::CoreStage::CleanUp, Engine::shutdown>();
+        // clang-format on
+
+        {
+            profiling::Stopwatch timer;
+            m_schedule.initialize(m_registry);
+            WS_LOG_INFO("Application",
+                        "Initialization took {} ms",
+                        timer.elapsedMs());
+        }
+    }
+
+    Application::~Application()
+    {
+        m_schedule.shutdown(m_registry);
+    }
+
+    void Application::run()
+    {
+        while (!Window::shouldClose())
+        {
+            m_schedule.run(m_registry);
+        }
+    }
+
+} // namespace worse::pc
--- Engine/Programs/Entry.cpp
+++ Engine/Programs/Entry.cpp
@@ -1,53 +1,12 @@
 #include "Log.hpp"
-#include "Engine.hpp"
-#include "Profiling/Stopwatch.hpp"
-#include "Renderer.hpp"
+#include "Application.hpp"
 
-#include "ECS/Registry.hpp"
-#include "ECS/Schedule.hpp"
-
-#include "World.hpp"
-
-#include <cstring>
-
-using namespace worse;
-
-int main()
+int main(int argc, char** argv)
 {
-    Logger::initialize();
-    ecs::Registry registry;
-    ecs::Schedule schedule;
+    worse::Logger::initialize();
 
-    schedule.addSystem<ecs::CoreStage::StartUp, &Engine::initialize>();
-    schedule.addSystem<ecs::CoreStage::StartUp, &Renderer::initialize>();
-    schedule.addSystem<ecs::CoreStage::StartUp, &ImGuiRenderer::initialize>();
-    schedule.addSystem<ecs::CoreStage::StartUp, &World::initialize>();
-    schedule.addSystem<ecs::CoreStage::StartUp, &World::setupScene>();
-    schedule.addSystem<ecs::CoreStage::StartUp, buildMeshes>();
-    schedule.addSystem<ecs::CoreStage::StartUp, buildMaterials>();
+    worse::pc::Application app(argc, argv);
+    app.run();
 
-    schedule.addSystem<ecs::CoreStage::Update, &Engine::tick>();
-    schedule.addSystem<ecs::CoreStage::Update, &World::updateInput>();
-    schedule.addSystem<ecs::CoreStage::Update, &World::update>();
-    schedule.addSystem<ecs::CoreStage::Update, buildDrawcalls>();
-    schedule.addSystem<ecs::CoreStage::Update, &ImGuiRenderer::tick>();
-    schedule.addSystem<ecs::CoreStage::Update, &Renderer::tick>();
-
-    schedule.addSystem<ecs::CoreStage::CleanUp, &ImGuiRenderer::shutdown>();
-    schedule.addSystem<ecs::CoreStage::CleanUp, &Renderer::shutdown>();
-    schedule.addSystem<ecs::CoreStage::CleanUp, &Engine::shutdown>();
-
-    profiling::Stopwatch mainTimer;
-    schedule.initialize(registry);
-    WS_LOG_INFO("Main",
-                "ECS schedule initialized in {} ms",
-                mainTimer.elapsedMs());
-
-    while (!Window::shouldClose())
-    {
-        schedule.run(registry);
-    }
-    schedule.shutdown(registry);
-
-    Logger::shutdown();
+    worse::Logger::shutdown();
 }
